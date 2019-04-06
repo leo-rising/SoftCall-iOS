2f28e91188d6476279a420314ce1f0a09dac0315
K F <kellyfesler@gmail.com>
2019-04-06 14:11:49 -0700

    fixed very bad rookie mistake

--- twilio-tests/nodejs/make_call.js
+++ twilio-tests/nodejs/make_call.js
@@ -3,8 +3,8 @@
 // Download the helper library from https://www.twilio.com/docs/node/install
 // Your Account Sid and Auth Token from twilio.com/console
 // DANGER! This is insecure. See http://twil.io/secure
-const accountSid = 'ACd86cb45b3b80e94323a3c29e9466d1ee';
-const authToken = 'd1efabb19786f56d13052595ab8f0b90';
+const accountSid = '';
+const authToken = '';
 const client = require('twilio')(accountSid, authToken);
 
 //var express = require('express');
